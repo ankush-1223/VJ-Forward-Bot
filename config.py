35316dd91488046d7eaf927ff6a64c29dadd6282
ankush-1223 <rdxankush23@gmail.com>
2025-04-08 00:43:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     API_HASH = environ.get("API_HASH", "31c9bbed9c688b89736d94da7e89653b")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
-    DATABASE_URI = environ.get("DATABASE_URI", "019253983:AAGIOzaCCdIG45Wf5KdFsMT3d1nu5Awcx9Q")
+    DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://ankush23:JYzSGlbjpFezDSWH@cluster0.ulzzd1m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
     BOT_OWNER = int(environ.get("BOT_OWNER", "7526345865"))
 
